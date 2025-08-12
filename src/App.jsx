1814a3089b42bbb9d52b11f0f90a7f4ebe6126b1
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2025-08-12 22:55:42 +0500

    Changes of Routes for deploying

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
-import Home from "./Pages/home_page/Home"
-import Login from "./Pages/login_page/login"
-import Admin from "./Pages/admin_page/admin"
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import Home from "./Pages/home_page/Home";
+import Login from "./Pages/login_page/login";
+import Admin from "./Pages/admin_page/admin";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Navigate to="/login" replace />
-    },
-    {
-      path: "/login",
-      element: <Login />
-    },
-    {
-      path: "/home",
-      element: <Home />
-    },
-    {
-      path: "/admin",
-      element: <Admin />
-    }
-  ], {
-    basename: "/Bakery-Website" 
-  });
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/admin",
+        element: <Admin />,
+      },
+    ]
+  );
+
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
